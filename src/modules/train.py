b74717da771da203bb0a014bb6575466cef30a99
libanliam <90952687+libanliam@users.noreply.github.com>
2023-11-05 23:16:21 +0800

    Print Time

--- src/modules/train.py
+++ src/modules/train.py
@@ -1,12 +1,14 @@
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
+import time
 
 def train_model(train_loader, val_loader, combined_model, criterion, optimizer, device, num_epochs):
     train_losses = []  # To store training losses for each epoch
     val_losses = []    # To store validation losses for each epoch
 
     for epoch in range(num_epochs):
+        start_time = time.time()
         combined_model.train()
         total_train_loss = 0
 
@@ -49,11 +51,14 @@ def train_model(train_loader, val_loader, combined_model, criterion, optimizer, 
                 val_loss = criterion(combined_output, target_labels)
                 total_val_loss += val_loss.item()
 
+        end_time = time.time()  # Record the end time for the epoch
+        epoch_time = end_time - start_time  # Calculate the time taken for the epoch
+        
         average_val_loss = total_val_loss / len(val_loader)
         val_losses.append(average_val_loss)
 
         combined_model.train()  # Set the model back to training mode
 
-        print(f'Epoch [{epoch+1}/{num_epochs}], Training Loss: {average_train_loss:.4f}, Validation Loss: {average_val_loss:.4f}')
+        print(f'Epoch [{epoch+1}/{num_epochs}], Training Loss: {average_train_loss:.4f}, Validation Loss: {average_val_loss:.4f}, Time: {epoch_time:.2f} seconds')
 
     return train_losses, val_losses
