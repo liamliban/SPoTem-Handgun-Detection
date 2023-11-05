0246b6aab853d39e812f45740bbc6441110a3b2a
rlribaya <79568279+rlribaya@users.noreply.github.com>
2023-11-06 00:52:46 +0800

    Print Accuracy

--- src/modules/train.py
+++ src/modules/train.py
@@ -11,6 +11,8 @@ def train_model(train_loader, val_loader, combined_model, criterion, optimizer, 
         start_time = time.time()
         combined_model.train()
         total_train_loss = 0
+        correct = 0
+        total = 0
 
         for batch in train_loader:
             data_name, gun_data, pose_data, motion_data, target_labels = batch
@@ -24,11 +26,17 @@ def train_model(train_loader, val_loader, combined_model, criterion, optimizer, 
 
             combined_output = combined_model(gun_data, pose_data, motion_data)
 
+            _, predicted = torch.max(combined_output, 1)  # Get the class with the highest probability
+            total += target_labels.size(0)  # Accumulate the total number of examples
+            correct += (predicted == target_labels).sum().item()  # Count correct predictions
+
             loss = criterion(combined_output, target_labels)
             loss.backward()
             optimizer.step()
 
             total_train_loss += loss.item()
+
+        train_accuracy = 100 * correct / total
 
         average_train_loss = total_train_loss / len(train_loader)
         train_losses.append(average_train_loss)
@@ -36,6 +44,8 @@ def train_model(train_loader, val_loader, combined_model, criterion, optimizer, 
         # Validation loop
         combined_model.eval()  # Set the model to evaluation mode
         total_val_loss = 0
+        correct = 0
+        total = 0
 
         with torch.no_grad():
             for batch in val_loader:
@@ -48,8 +58,14 @@ def train_model(train_loader, val_loader, combined_model, criterion, optimizer, 
 
                 combined_output = combined_model(gun_data, pose_data, motion_data)
 
+                _, predicted = torch.max(combined_output, 1)  # Get the class with the highest probability
+                total += target_labels.size(0)  # Accumulate the total number of examples
+                correct += (predicted == target_labels).sum().item()  # Count correct predictions
+
                 val_loss = criterion(combined_output, target_labels)
                 total_val_loss += val_loss.item()
+
+        val_accuracy = 100 * correct / total
 
         end_time = time.time()  # Record the end time for the epoch
         epoch_time = end_time - start_time  # Calculate the time taken for the epoch
@@ -59,6 +75,6 @@ def train_model(train_loader, val_loader, combined_model, criterion, optimizer, 
 
         combined_model.train()  # Set the model back to training mode
 
-        print(f'Epoch [{epoch+1}/{num_epochs}], Training Loss: {average_train_loss:.4f}, Validation Loss: {average_val_loss:.4f}, Time: {epoch_time:.2f} seconds')
+        print(f'Epoch [{epoch+1}/{num_epochs}], Training Accuracy: {train_accuracy:.2f}%, Training Loss: {average_train_loss:.4f}, Validation Accuracy: {val_accuracy:.2f}%, Validation Loss: {average_val_loss:.4f}, Time: {epoch_time:.2f} seconds')
 
     return train_losses, val_losses
