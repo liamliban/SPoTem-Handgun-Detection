bb7f1fc0dd0002b9de6816224db5dde932c56920
JM-Flores <juanmiguel.flores.cics@ust.edu.ph>
2023-11-12 18:02:54 +0800

    gun yolo

--- src/modules/gun_yolo.py
+++ src/modules/gun_yolo.py
@@ -78,28 +78,6 @@ class CustomDarknet53(nn.Module):
         return x
 
 
-class CustomDarknet53_Standalone(nn.Module):
-    def __init__(self, darknet_model):
-        super(CustomDarknet53_Standalone, self).__init__()
-
-        # Load the pretrained darknet53 model
-        self.darknet = darknet_model
-
-        # Remove the last linear layer
-        self.darknet2 = torch.nn.Sequential(*list(self.darknet.children())[:-1])
-
-        # freeze all parameters of the model
-        for param in self.darknet2.parameters():
-            param.requires_grad = False
-
-        # Add a final linear layer
-        self.dense = nn.Linear(1024, 2)
-
-    def forward(self, x):
-        x = self.darknet2(x)
-        x = self.dense(x)
-        return x
-
 class GunLSTM(nn.Module):
     def __init__(self, darknet_model):
         super(GunLSTM, self).__init__()
@@ -121,6 +99,8 @@ class GunLSTM(nn.Module):
             num_layers=1,
             batch_first=True)
         
+        # self.dropout = nn.Dropout(0.4)
+        
         # self.linear = nn.Linear(20,20)
 
     def forward(self, x):
@@ -130,125 +110,73 @@ class GunLSTM(nn.Module):
         r_in = c_out.view(batch_size, timesteps, -1)
         r_out, (h_n, h_c) = self.lstm(r_in)
         r_out = r_out[:, -1, :]
+
+        # r_out = self.dropout(r_out)
         # r_out2 = self.linear(r_out)
         
         return r_out
-
-
-
-# class Conv(nn.Module):
-#     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):
-#         super(Conv, self).__init__()
-#         self.conv = nn.Conv2d(c1, c2, k, s, auto_pad(k, p), groups=g, bias=False)
-#         self.bn = nn.BatchNorm2d(c2)
-#         self.act = nn.LeakyReLU(0.01) if act else nn.ReLU()
-
-#     def forward(self, x):
-#         return self.act(self.bn(self.conv(x)))
-
-
-# class ResidualBlock(nn.Module):
-#     def __init__(self, c1, shortcut=True):
-#         super(ResidualBlock, self).__init__()
-#         c2 = c1 // 2
-#         self.shortcut = shortcut
-#         self.layer1 = Conv(c1, c2, p=0)
-#         self.layer2 = Conv(c2, c1, k=3)
-
-#     def forward(self, x):
-#         residual = x
-#         out = self.layer1(x)
-#         out = self.layer2(out)
-#         out += residual
-#         if self.shortcut:
-#             out += residual
-#         return out
     
-# class DarkNet53FeatureExtractor(nn.Module):
-#     def __init__(self, block):
-#         super(DarkNet53FeatureExtractor, self).__init__()
+# class CustomDarknet53_NoDense(nn.Module):
+#     def __init__(self, darknet_model):
+#         super(CustomDarknet53_NoDense, self).__init__()
 
-#         self.features = nn.Sequential(
-#             Conv(3, 32, 3),
-#             Conv(32, 64, 3, 2),
-#             *self._make_layer(block, 64, num_blocks=1),
-#             Conv(64, 128, 3, 2),
-#             *self._make_layer(block, 128, num_blocks=2),
-#             Conv(128, 256, 3, 2),
-#             *self._make_layer(block, 256, num_blocks=8),
-#             Conv(256, 512, 3, 2),
-#             *self._make_layer(block, 512, num_blocks=8),
-#             Conv(512, 1024, 3, 2),
-#             *self._make_layer(block, 1024, num_blocks=4),
-#             GlobalAvgPool2d()
-#         )
+#         # Load the pretrained darknet53 model
+#         self.darknet = darknet_model
 
-#         self.final_linear = nn.Linear(1024, 20)
+#         # Remove the last linear layer
+#         self.darknet2 = torch.nn.Sequential(*list(self.darknet.children())[:-1])
+
+#         # freeze all parameters of the model
+#         for param in self.darknet2.parameters():
+#             param.requires_grad = False
+
 
 #     def forward(self, x):
-#         x = self.features(x)
-#         x = self.final_linear(x)
+#         x = self.darknet2(x)
 #         return x
 
-#     @staticmethod
-#     def _make_layer(block, in_channels, num_blocks):
-#         layers = []
-#         for i in range(0, num_blocks):
-#             layers.append(block(in_channels))
-#         return nn.Sequential(*layers)
+# class GunLSTM_Optimized(nn.Module): #instead of running darknet53 inside, take the tensors from previously evaluated hand images instead
+#     def __init__(self):
+#         super(GunLSTM_Optimized, self).__init__()
 
-# class DarkNet53(nn.Module):
-#     """ [https://pjreddie.com/media/files/papers/YOLOv3.pdf] """
-#     def __init__(self, block, num_classes=1000, init_weight=True):
-#         super(DarkNet53, self).__init__()
-#         self.num_classes = num_classes
-
-#         if init_weight:
-#             self._initialize_weights()
-
-#         self.features = nn.Sequential(
-#             Conv(3, 32, 3),
-
-#             Conv(32, 64, 3, 2),
-#             *self._make_layer(block, 64, num_blocks=1),
-
-#             Conv(64, 128, 3, 2),
-#             *self._make_layer(block, 128, num_blocks=2),
-
-#             Conv(128, 256, 3, 2),
-#             *self._make_layer(block, 256, num_blocks=8),
-
-#             Conv(256, 512, 3, 2),
-#             *self._make_layer(block, 512, num_blocks=8),
-
-#             Conv(512, 1024, 3, 2),
-#             *self._make_layer(block, 1024, num_blocks=4)
-#         )
-#         self.classifier = nn.Sequential(
-#             *self.features,
-#             GlobalAvgPool2d(),
-#             nn.Linear(1024, num_classes)
-#         )
+#         self.lstm = nn.LSTM(
+#             input_size=1024, 
+#             hidden_size=20, 
+#             num_layers=1,
+#             batch_first=True)
+        
+#         # self.dropout = nn.Dropout(0.2)
+        
+#         # self.linear = nn.Linear(20,20)
 
 #     def forward(self, x):
-#         return self.classifier(x)
+        
+#         r_out, (h_n, h_c) = self.lstm(x)
+#         r_out = r_out[:, -1, :]
+#         # r_out = self.dropout(r_out)
+#         # r_out2 = self.linear(r_out)
+        
+#         return r_out
 
-#     def _initialize_weights(self):
-#         for m in self.modules():
-#             if isinstance(m, nn.Conv2d):
-#                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
-#                 if m.bias is not None:
-#                     nn.init.constant_(m.bias, 0)
-#             elif isinstance(m, nn.BatchNorm2d):
-#                 nn.init.constant_(m.weight, 1)
-#                 nn.init.constant_(m.bias, 0)
-#             elif isinstance(m, nn.Linear):
-#                 nn.init.normal_(m.weight, 0, 0.01)
-#                 nn.init.constant_(m.bias, 0)
+# class GunLSTM_Optimized(nn.Module): #instead of running darknet53 inside, take the tensors from previously evaluated hand images instead
+#     def __init__(self):
+#         super(GunLSTM_Optimized, self).__init__()
+#         self.num_layers = 1
+#         self.hidden_size = 20
+#         self.lstm = nn.LSTM(
+#             input_size=1024, 
+#             hidden_size=self.hidden_size, 
+#             num_layers=self.num_layers,
+#             batch_first=True)
+#         # self.fc = nn.Linear(self.hidden_size, self.output_size)
 
-#     @staticmethod
-#     def _make_layer(block, in_channels, num_blocks):
-#         layers = []
-#         for i in range(0, num_blocks):
-#             layers.append(block(in_channels))
-#         return nn.Sequential(*layers)
+#     def forward(self, x):
+#         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+#         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+#         out, _ = self.lstm(x, (h0, c0))
+        
+#         # Select the last time step's output
+#         out = out[:, -1, :]
+#         # out = self.fc(out)
+#         return out
