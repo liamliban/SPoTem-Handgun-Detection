0538ba5da8d84306e83280a3ba8a5f8bad16b937
rlribaya <79568279+rlribaya@users.noreply.github.com>
2023-11-22 12:50:32 +0800

    Add Cross Validation

---
+++ main_cross_validate.py
@@ -0,0 +1,221 @@
+import pandas as pd
+import torch
+import os
+from src.modules import motion_analysis
+from yolo.pytorchyolo import models
+import torchvision.transforms as transforms
+from src.modules.posecnn import poseCNN
+from src.modules.gun_yolo import CustomDarknet53, GunLSTM, GunLSTM_Optimized, Gun_Optimized
+from src.modules.combined_model import CombinedModel, CombinedModelNewVer
+from src.modules.combined_model_no_motion import CombinedModelNoMotion
+from src.modules.custom_dataset import CustomGunDataset
+from src.modules.custom_dataset_gunLSTM import CustomGunLSTMDataset
+from src.modules.custom_dataset_gunLSTM_opt import CustomGunLSTMDataset_opt
+from src.modules.custom_dataset_opt import CustomGunDataset_opt
+from src.modules.train import train_model
+from src.modules.cross_validate import cross_validate
+from torch.utils.data import DataLoader, Subset
+import torch.optim as optim
+from sklearn.model_selection import train_test_split, KFold
+import matplotlib.pyplot as plt
+import seaborn as sns
+from holocron.models import darknet53
+import numpy as np
+import random
+
+# Set a random seed for reproducibility
+torch.manual_seed(12)
+torch.cuda.manual_seed(12)
+np.random.seed(12)
+random.seed(12)
+os.environ['PYTHONHASHSEED'] = str(12)
+torch.cuda.manual_seed_all(12)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.enabled = False
+
+torch.backends.cudnn.deterministic=True
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+print("Device: " , device)
+
+
+# Load the models
+darknet_model = darknet53(pretrained=True)
+pose_model = poseCNN()
+
+hidden_size = 100
+window_size = 5
+lstm_layers = 1
+batch_size = 16
+learning_rate = 1e-5
+num_epochs = 2
+
+user_input =  0
+model_name = ''
+while True:
+    user_input = input("Do you want to train GPM (1), GP (2), GPM2 (3), GPM2-opt (4), GP-opt (5), GPM-opt (6)? Enter '1', '2', '3', '4', '5', '6': ").strip().upper()
+    if user_input == '1':
+        gun_model = CustomDarknet53(darknet_model)
+        motion_model = motion_analysis.MotionLSTM(hidden_size, lstm_layers)
+        combined_feature_size = 20 + 20 + hidden_size #total num of features of 3 model outputs
+        model_name = 'GPM'
+        break
+    elif user_input == '2':
+        gun_model = CustomDarknet53(darknet_model)
+        combined_feature_size = 20 + 20 #total num of features of 3 model outputs
+        model_name = 'GP'
+        break
+    elif user_input == '3':
+        gun_model = GunLSTM(darknet_model, hidden_size=hidden_size)
+        combined_feature_size = 20 + hidden_size #total num of features of 3 model outputs
+        model_name = 'GPM2'
+        break
+    elif user_input == '4':
+        gun_model = GunLSTM_Optimized(hidden_size, lstm_layers)
+        combined_feature_size = 20 + hidden_size #total num of features of 3 model outputs
+        model_name = 'GPM2-opt'
+        break
+    elif user_input == '5':
+        gun_model = Gun_Optimized()
+        combined_feature_size = 20 + 20 #total num of features of 3 model outputs
+        model_name = 'GP-opt'
+        break
+    elif user_input == '6':
+        gun_model = Gun_Optimized()
+        motion_model = motion_analysis.MotionLSTM(hidden_size, lstm_layers)
+        combined_feature_size = 20 + 20 + hidden_size #total num of features of 3 model outputs
+        model_name = 'GPM-opt'
+        break
+    else:
+        print("Invalid input. Please enter '1' for all three models or '2' for combined model with no motion or '3' for new motion model.")
+
+if user_input == '3': 
+    # DATASET FOR NEW MODEL
+    custom_dataset = CustomGunLSTMDataset(root_dir='data', window_size = window_size)
+elif user_input == '4': 
+    # DATASET FOR NEW MODEL optimized
+    custom_dataset = CustomGunLSTMDataset_opt(root_dir='data2', window_size = window_size)
+elif user_input == '5' or user_input == '6': 
+    # DATASET FOR old model optimized
+    custom_dataset = CustomGunDataset_opt(root_dir='data', window_size = window_size)
+else:    
+    custom_dataset = CustomGunDataset(root_dir='data', window_size = window_size)
+
+print ("Number of samples in dataset: ", len(custom_dataset))
+
+label_0 = 0
+label_1 = 0
+
+for idx, data_entry in enumerate(custom_dataset.data):
+    label = data_entry["label"]
+    if label == '0':
+        label_0+=1
+    elif label == '1':
+        label_1+=1
+
+print ("Number of label 0: ", label_0)
+print ("Number of label 1: ", label_1)
+
+# Set the device
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+excel_filename = 'logs/results.xlsx'
+
+train_loss = []
+val_loss = []
+folds = 5
+kf = KFold(n_splits=folds, random_state=42, shuffle=True)
+for fold_num, (train_indices, val_indices) in enumerate(kf.split(custom_dataset)):
+    
+    # (Re)Initialize model
+    if user_input == '1':
+        combined_model = CombinedModel(gun_model, pose_model, motion_model, combined_feature_size)
+    elif user_input == '2':
+        combined_model = CombinedModelNoMotion(gun_model, pose_model, combined_feature_size)
+    elif user_input == '3':
+        combined_model = CombinedModelNewVer(gun_model, pose_model, combined_feature_size)
+    elif user_input == '4':
+        combined_model = CombinedModelNewVer(gun_model, pose_model, combined_feature_size)
+    elif user_input == '5':
+        combined_model = CombinedModelNewVer(gun_model, pose_model, combined_feature_size)
+    elif user_input == '6':
+        combined_model = CombinedModel(gun_model, pose_model, motion_model, combined_feature_size)
+
+    combined_model.to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.Adam(combined_model.parameters(), lr=learning_rate)
+
+    train_dataset = Subset(dataset=custom_dataset, indices=train_indices)
+    val_dataset = Subset(dataset=custom_dataset, indices=val_indices)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    # Print Model Info
+    print("\n")
+    model_info = {
+        'fold'          : fold_num,
+        'model_type'    : model_name,
+        'window_size'   : window_size,
+        'hidden_size'   : hidden_size,
+        'lstm_layers'   : lstm_layers,
+        'train_set_size': len(train_dataset),
+        'val_set_size'  : len(val_dataset),
+        'batch_size'    : batch_size,
+        'criterion'     : criterion.__class__.__name__,
+        'optimizer'     : optimizer.__class__.__name__,
+        'learning_rate' : optimizer.param_groups[0]['lr'],
+        'epochs'        : num_epochs
+    }
+    df = pd.DataFrame([model_info]).T.rename(columns={0: 'Value'})
+    df = df.reset_index().rename(columns={'index': 'Hyperparameter'})
+    print(f'\n{df.to_string(index=False)}\n')
+
+    run_number, trained_model, train_losses, val_losses = cross_validate(
+        user_input, 
+        train_loader, 
+        val_loader, 
+        combined_model, 
+        criterion, 
+        optimizer, 
+        device, 
+        num_epochs, 
+        excel_filename,
+        fold_num,
+        save=True,
+        model_info=model_info,
+        save_excel=fold_num+1==folds
+    )
+    train_loss.append(train_losses)
+    val_loss.append(val_losses)
+
+    # Look into incorrect predictions
+    incorrect_predictions = []
+    video_loader = DataLoader(val_dataset)
+    trained_model.eval()
+    with torch.no_grad():
+        for video in video_loader:
+            data_name, gun_data, pose_data, motion_data, target_label = video
+
+            gun_data = gun_data.to(device)
+            pose_data = pose_data.to(device)
+            motion_data = motion_data.to(device)
+            target_label = target_label.to(device)
+            
+            if user_input == '1' or user_input == '6':
+                combined_output = trained_model(gun_data, pose_data, motion_data)
+            else:
+                combined_output = trained_model(gun_data, pose_data)
+            
+            predicted_label = torch.argmax(combined_output)
+            if predicted_label != target_label:
+                incorrect_predictions.append({'data_name': data_name[0], 'predicted_label': predicted_label, 'target_label': target_label})
+
+    # Save Incorrect Predictions
+    incorrect_predictions_path = f'logs/run#{run_number}/fold{fold_num+1}/IncorrectPredictions.txt'
+    with open(incorrect_predictions_path, 'w') as file:
+        for item in incorrect_predictions:
+            file.write(item['data_name'] + ':\n')
+            file.write(f'  Predicted Label: {item["predicted_label"]}\n')
+            file.write(f'  Correct Label: {item["target_label"]}\n\n')
+        print(f'Incorrect Predictions saved to: {incorrect_predictions_path}')
---
+++ src/modules/cross_validate.py
@@ -0,0 +1,306 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import time
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import pandas as pd
+import os
+from datetime import datetime
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+# Set a random seed for reproducibility
+torch.manual_seed(12)
+torch.cuda.manual_seed(12)
+np.random.seed(12)
+random.seed(12)
+os.environ['PYTHONHASHSEED'] = str(12)
+torch.cuda.manual_seed_all(12)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.enabled = False
+
+torch.backends.cudnn.deterministic=True
+
+def cross_validate( user_input, 
+                    train_loader, 
+                    val_loader, 
+                    combined_model, 
+                    criterion, 
+                    optimizer, 
+                    device, 
+                    num_epochs, 
+                    excel_filename, 
+                    fold_num=None, 
+                    save=False, 
+                    model_info=None,
+                    save_excel=False):
+
+    train_losses = []  # To store training losses for each epoch
+    val_losses = []    # To store validation losses for each epoch
+    train_accuracies = []
+    val_accuracies = []
+    outputs = []       # To store per epoch data
+
+    # Get the current date and time
+    current_datetime = datetime.now().strftime('%Y-%m-d %H:%M:%S')
+
+    # Get model name
+    model_name = ''
+    if user_input == '1':
+        model_name = 'GPM'
+    elif user_input == '2':
+        model_name = 'GP'
+    elif user_input == '3':
+        model_name = 'GPM2'
+    elif user_input == '4':
+        model_name = 'GPM2-opt'
+    elif user_input == '5':
+        model_name = 'GP-opt'
+    elif user_input == '6':
+        model_name = 'GPM-opt'
+
+
+    # Get the run number based on the existing Excel file
+    run_number = 1
+    if os.path.exists(excel_filename):
+        existing_df = pd.read_excel(excel_filename)
+        run_number = len(existing_df) + 1
+
+    log_folder = f'logs/run#{run_number}/fold{fold_num+1}/'
+
+    # Create The Logs Folder
+    if not os.path.exists(log_folder): 
+        os.makedirs(log_folder)
+
+    log_folder = f'logs/run#{run_number}/fold{fold_num+1}/'
+
+    print("Training Started: \n")
+
+    for epoch in range(num_epochs):
+        start_time = time.time()
+        combined_model.train()
+        total_train_loss = 0
+        correct = 0
+        total = 0
+        train_predictions = []
+        train_targets = []
+        numBatches = len(train_loader)
+        for index, batch in enumerate(train_loader):
+            data_name, gun_data, pose_data, motion_data, target_labels = batch
+
+            print(f'Epoch [{epoch+1}/{num_epochs}] {index}/{numBatches}', end='\r')
+
+            gun_data = gun_data.to(device)
+            pose_data = pose_data.to(device)
+            motion_data = motion_data.to(device)
+            target_labels = target_labels.to(device)
+
+            optimizer.zero_grad()
+
+            if user_input == '1' or user_input == '6':
+                combined_output = combined_model(gun_data, pose_data, motion_data)
+            else:
+                combined_output = combined_model(gun_data, pose_data)
+
+            _, predicted = torch.max(combined_output, 1)  # Get the class with the highest probability
+            total += target_labels.size(0)  # Accumulate the total number of examples
+            correct += (predicted == target_labels).sum().item()  # Count correct predictions
+
+            loss = criterion(combined_output, target_labels)
+            loss.backward()
+
+            # nn.utils.clip_grad_norm_(combined_model.parameters(), max_norm=2)
+
+            optimizer.step()
+
+            total_train_loss += loss.item()
+            train_predictions.extend(predicted.cpu().numpy())
+            train_targets.extend(target_labels.cpu().numpy())
+
+        print(f'Epoch [{epoch+1}/{num_epochs}] {numBatches}/{numBatches}', end='\r')
+        train_accuracy = 100 * correct / total
+        train_precision = precision_score(train_targets, train_predictions, average='weighted')
+        train_recall = recall_score(train_targets, train_predictions, average='weighted')
+        train_f1_score = f1_score(train_targets, train_predictions, average='weighted')
+        train_confusion_matrix = confusion_matrix(train_targets, train_predictions)
+
+        average_train_loss = total_train_loss / len(train_loader)
+        train_losses.append(average_train_loss)
+        train_accuracies.append(train_accuracy)
+
+        # Validation loop
+        combined_model.eval()  # Set the model to evaluation mode
+        total_val_loss = 0
+        correct = 0
+        total = 0
+        val_predictions = []
+        val_targets = []
+
+        with torch.no_grad():
+            for batch in val_loader:
+                data_name, gun_data, pose_data, motion_data, target_labels = batch
+
+                gun_data = gun_data.to(device)
+                pose_data = pose_data.to(device)
+                motion_data = motion_data.to(device)
+                target_labels = target_labels.to(device)
+                
+                if user_input == '1' or user_input == '6':
+                    combined_output = combined_model(gun_data, pose_data, motion_data)
+                else:
+                    combined_output = combined_model(gun_data, pose_data)
+                
+                _, predicted = torch.max(combined_output, 1)  # Get the class with the highest probability
+                total += target_labels.size(0)  # Accumulate the total number of examples
+                correct += (predicted == target_labels).sum().item()  # Count correct predictions
+
+                val_loss = criterion(combined_output, target_labels)
+                total_val_loss += val_loss.item()
+                val_predictions.extend(predicted.cpu().numpy())
+                val_targets.extend(target_labels.cpu().numpy())
+
+        val_accuracy = 100 * correct / total
+        val_precision = precision_score(val_targets, val_predictions, average='weighted')
+        val_recall = recall_score(val_targets, val_predictions, average='weighted')
+        val_f1_score = f1_score(val_targets, val_predictions, average='weighted')
+        val_confusion_matrix = confusion_matrix(val_targets, val_predictions)
+
+        end_time = time.time()  # Record the end time for the epoch
+        epoch_time = end_time - start_time  # Calculate the time taken for the epoch
+        
+        average_val_loss = total_val_loss / len(val_loader)
+        val_losses.append(average_val_loss)
+        val_accuracies.append(val_accuracy)
+
+        combined_model.train()  # Set the model back to training mode
+
+        output =  "Epoch [{}/{}], Training Accuracy: {:.2f}%, Training Loss: {:.4f}, Validation Accuracy: {:.2f}%, Validation Loss: {:.4f}, Time: {:.2f} seconds\n".format(epoch + 1, num_epochs, train_accuracy, average_train_loss, val_accuracy, average_val_loss, epoch_time)
+        output += "Training Precision: {:.4f}, Training Recall: {:.4f}, Training F1 Score: {:.4f}\n".format(train_precision, train_recall, train_f1_score)
+        output += "Validation Precision: {:.4f}, Validation Recall: {:.4f}, Validation F1 Score: {:.4f}\n".format(val_precision, val_recall, val_f1_score)
+        print(output + '\n', end='\r')
+
+        outputs.append(output) # collect outputs
+
+    # Save Model Last Epoch
+    if save:
+        model_checkpoint = {
+            'epoch': epoch,
+            'model_state_dict': combined_model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss
+        }
+        if model_info is not None:
+            model_checkpoint['model_info'] = model_info
+        model_folder = f'{log_folder}model/'
+        if not os.path.exists(model_folder):
+            os.makedirs(model_folder)
+        model_path = f'{model_folder}model_epoch_{epoch}.pt'
+        torch.save(model_checkpoint, model_path)
+        print(f'model saved in: {model_path}')
+
+    # Save per Epoch data
+    output_log_path = f'{log_folder}run#{run_number}_OutputLog.txt'
+    with open(output_log_path, 'w') as file:
+        model_architecture = '\t' + (str(combined_model)).replace("\n", "\n\t")
+        file.write(f'Model Architecture:\n{model_architecture}\n')
+        if model_info is not None:
+            file.write('\nHyperparameters:\n')
+            for key, value in model_info.items():
+                file.write(f'\t{key}\t: {value}\n')
+        file.write('Per Epoch Output:\n')
+        for output in outputs:
+            file.write('\t' + output.replace('\n', '\n\t') + '\n')
+
+        print(f'Output log saved to: {output_log_path}')
+
+    # Show and Save Loss Plot
+    loss_path = f'{log_folder}run#{run_number}_loss.png'
+
+    plt.plot(train_losses, label='Training Loss')
+    plt.plot(val_losses, label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.title(f'{model_name} Training and Validation Loss')
+    plt.savefig(loss_path)
+    print(f'Loss Diagram Saved To: {loss_path}')
+    plt.close()
+
+    # Show and Save Accuracy Plot
+    acc_path = f'{log_folder}run#{run_number}_accuracy.png'
+
+    plt.plot(train_accuracies, label='Training Accuracy')
+    plt.plot(val_accuracies, label='Validation Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.title(f'{model_name} Training and Validation Accuracy')
+    plt.savefig(acc_path)
+    print(f'Accuracy Diagram Saved To: {acc_path}')
+    plt.close()
+
+    # Save the results to Excel with run number and date
+    if save_excel:
+        write_results_to_excel(excel_filename, run_number, current_datetime, user_input, num_epochs, train_accuracy, train_precision, train_recall, train_f1_score, val_accuracy, val_precision, val_recall, val_f1_score, train_losses, val_losses)
+
+    return run_number, combined_model, train_losses, val_losses
+
+# Function to write the results to Excel
+def write_results_to_excel(filename, run_number, current_datetime, user_input, num_epochs, train_accuracy, train_precision, train_recall, train_f1, val_accuracy, val_precision, val_recall, val_f1, train_losses, val_losses):
+    if not os.path.exists(filename):
+        df = pd.DataFrame(columns=["Run #", "Date", "Model", "Epochs", "Train Accuracy", "Train Loss", "Train Precision", "Train Recall", "Train F1", "Val Accuracy", "Val Loss", "Val Precision", "Val Recall", "Val F1"])
+    else:
+        df = pd.read_excel(filename)
+    model = ''
+    if user_input == '1':
+        model = 'GPM'
+    elif user_input == '2':
+        model = 'GP'
+    elif user_input == '3':
+        model = 'GPM2'
+    elif user_input == '4':
+        model = 'GPM2-opt'
+    elif user_input == '5':
+        model = 'GP-opt'
+    elif user_input == '6':
+        model = 'GPM-opt'
+
+    # Format the accuracy with 2 decimal points and others with at most 4 decimal points
+    train_accuracy_str = "{:.2f}%".format(train_accuracy)
+    train_precision_str = "{:.4f}".format(train_precision)
+    train_recall_str = "{:.4f}".format(train_recall)
+    train_f1_str = "{:.4f}".format(train_f1)
+    val_accuracy_str = "{:.2f}%".format(val_accuracy)
+    val_precision_str = "{:.4f}".format(val_precision)
+    val_recall_str = "{:.4f}".format(val_recall)
+    val_f1_str = "{:.4f}".format(val_f1)
+    train_loss_str = "{:.4f}".format(train_losses[-1]) 
+    val_loss_str = "{:.4f}".format(val_losses[-1])
+
+    new_row = pd.Series({"Run #": run_number,
+                         "Date": current_datetime,
+                         "Model": model,
+                         "Epochs": num_epochs,
+                         "Train Accuracy": train_accuracy_str,
+                         "Train Loss": train_loss_str,
+                         "Train Precision": train_precision_str,
+                         "Train Recall": train_recall_str,
+                         "Train F1": train_f1_str,
+                         "Val Accuracy": val_accuracy_str,
+                         "Val Loss": val_loss_str,
+                         "Val Precision": val_precision_str,
+                         "Val Recall": val_recall_str,
+                         "Val F1": val_f1_str})
+
+    df = df.append(new_row, ignore_index=True)
+    
+    # Create logs folder if missing
+    if not os.path.exists('logs/'):
+        os.makedirs("logs/")
+
+    df.to_excel(filename, index=False)
+
+    print ("Results saved to: ", filename)
+
